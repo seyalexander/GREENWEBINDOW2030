f160a333c7200a557982dc6460f4b4706578cf70
seyalexander <alexande1809piscis@gmail.com>
2025-01-13 01:21:35 -0500

    imagens de inicio carousel mejorado

--- src/app/UI/MODEL/INICIO/PAGE/inicio-page/inicio-page.component.ts
+++ src/app/UI/MODEL/INICIO/PAGE/inicio-page/inicio-page.component.ts
@@ -17,9 +17,9 @@ export class InicioPageComponent {
 
    imagen_a: string = "https://www.dropbox.com/scl/fi/u7427cpq0jux3qdtgvgyf/green-webindow.png?rlkey=tuzd1vj5al3mcer34bj0jochz&st=swqqocar&raw=1"
    imagen_b: string = "https://www.dropbox.com/scl/fi/jd0jf2m8kiuhxn8rx93xu/WhatsApp-Image-2024-12-28-at-3.38.02-PM-7.jpeg?rlkey=q5tbbe24zre67lxcb1mqjnsue&st=xp76p23r&raw=1"
-   imagen_c: string = "https://www.dropbox.com/scl/fi/grxpcoilrhyo60367p18v/1.jpg?rlkey=wl4d1kkjf76rgkqo73083qk2y&st=ty8tmcae&raw=1"
-   imagen_d: string = "https://images.pexels.com/photos/666870/pexels-photo-666870.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-   imagen_e: string = "https://images.pexels.com/photos/26201108/pexels-photo-26201108/free-photo-of-perro-mascota-cabeza-habitacion.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+   imagen_c: string = "https://www.dropbox.com/scl/fi/tpwmusewdp9rc2zi247qz/Imagen-de-WhatsApp-2025-01-06-a-las-22.47.24_9a22a43e.jpg?rlkey=3c4cs5q7xqzr9kuc3rfoqfgbo&st=tsj4k5du&raw=1"
+   imagen_d: string = "https://www.dropbox.com/scl/fi/dtfzrxlhcpmkyw83mflkm/Imagen-de-WhatsApp-2025-01-06-a-las-22.49.15_19d85958.jpg?rlkey=x020282rv35sh713gkrygxry8&st=m82dmrgt&raw=1"
+   imagen_e: string = "https://www.dropbox.com/scl/fi/jd0jf2m8kiuhxn8rx93xu/WhatsApp-Image-2024-12-28-at-3.38.02-PM-7.jpeg?rlkey=q5tbbe24zre67lxcb1mqjnsue&st=oa71ys38&raw=1"
 
 
   mainMenu: {
