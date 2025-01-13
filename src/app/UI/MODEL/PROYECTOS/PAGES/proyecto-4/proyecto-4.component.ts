e705f83e5ff446ac03b3c31d8c262743b087ec12
seyalexander <alexande1809piscis@gmail.com>
2025-01-13 00:50:33 -0500

    add imagenes caroucel proyecto 4

--- src/app/UI/MODEL/PROYECTOS/PAGES/proyecto-4/proyecto-4.component.ts
+++ src/app/UI/MODEL/PROYECTOS/PAGES/proyecto-4/proyecto-4.component.ts
@@ -11,9 +11,9 @@ import { CarouselComponent } from '../../../../SHARED/carousel/carousel.componen
   styleUrl: './proyecto-4.component.css'
 })
 export class Proyecto4Component {
-  imagen_a: string = "https://www.dropbox.com/scl/fi/u7427cpq0jux3qdtgvgyf/green-webindow.png?rlkey=tuzd1vj5al3mcer34bj0jochz&st=swqqocar&raw=1"
-  imagen_b: string = "https://www.dropbox.com/scl/fi/jd0jf2m8kiuhxn8rx93xu/WhatsApp-Image-2024-12-28-at-3.38.02-PM-7.jpeg?rlkey=q5tbbe24zre67lxcb1mqjnsue&st=xp76p23r&raw=1"
-  imagen_c: string = "https://www.dropbox.com/scl/fi/grxpcoilrhyo60367p18v/1.jpg?rlkey=wl4d1kkjf76rgkqo73083qk2y&st=ty8tmcae&raw=1"
-  imagen_d: string = "https://images.pexels.com/photos/666870/pexels-photo-666870.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-  imagen_e: string = "https://images.pexels.com/photos/26201108/pexels-photo-26201108/free-photo-of-perro-mascota-cabeza-habitacion.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  imagen_a: string = "https://www.dropbox.com/scl/fi/npqfi36l6zcnl3bfclw54/20241211_075622.jpg?rlkey=rl9rqn14dgd97xeohal28t3zb&st=klhp7puz&raw=1"
+  imagen_b: string = "https://www.dropbox.com/scl/fi/fivc6w2rxpyd4yb34xait/20241211_080440.jpg?rlkey=kbsemxriuumz9jctjff11zpf2&st=083zrfn4&raw=1"
+  imagen_c: string = "https://www.dropbox.com/scl/fi/giiwbh6jxd06ghpw8cbyh/20241211_080848.jpg?rlkey=xvg4nl6ore1tenw8anzw70max&st=3u3u2a0y&raw=1"
+  imagen_d: string = "https://www.dropbox.com/scl/fi/gufz3ibxfubgiq2jmnj74/IMG-20241211-WA0013.jpg?rlkey=h6d2v9r700gmx1egzhlsaac1n&st=1h8ar7db&raw=1"
+  imagen_e: string = "https://www.dropbox.com/scl/fi/0c0bpmrwdjeeaui0rx9l2/IMG-20241211-WA0017.jpg?rlkey=2urwqcbm6yfboi6tc512izuoj&st=76jmbipj&raw=1"
 }
