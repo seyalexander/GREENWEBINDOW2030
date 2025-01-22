a5df83bdcd6de5ceb60f8ed4402bed95717aca7e
seyalexander <alexande1809piscis@gmail.com>
2025-01-21 22:53:49 -0500

    tarjetas eventos

--- src/app/UI/MODEL/INICIO/PAGE/inicio-page/inicio-page.component.ts
+++ src/app/UI/MODEL/INICIO/PAGE/inicio-page/inicio-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {  Router, RouterLink } from '@angular/router';
+import {  Router } from '@angular/router';
 import { CarouselComponent } from '../../../../SHARED/carousel/carousel.component';
 import { TargetsModel2Component } from '../../COMPONENTS/targets/targets-model-2/targets-model-2.component';
 
@@ -8,7 +8,6 @@ import { TargetsModel2Component } from '../../COMPONENTS/targets/targets-model-2
   selector: 'app-inicio-page',
   standalone: true,
   imports: [
-    RouterLink,
     CarouselComponent,
     TargetsModel2Component
   ],
@@ -26,13 +25,20 @@ export class InicioPageComponent {
 
   mainMenu: {
     accessLink: Array<any>;
+    eventosLink: Array<any>;
   } = {
     accessLink: [],
+    eventosLink: []
   };
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.PROYECTOS()
+    this.EVENTOS()
+  }
+
+  PROYECTOS() {
     this.mainMenu.accessLink = [
       {
         name: 'PATHS OF VALUE',
@@ -82,6 +88,43 @@ export class InicioPageComponent {
         detalle: 'Pet project',
         route: ['/','proyecto-6']
       }
+    ]
+  }
+
+  EVENTOS() {
+    this.mainMenu.eventosLink = [
+      {
+        name: 'Marathon de lectures multilingues',
+        imagen: 'https://www.dropbox.com/scl/fi/wboum1avbk7do6jwm5dzh/PROYECTO-1.png?rlkey=w8vz8a9jak1mgz0z4p45v4bl1&st=2g877m6n&raw=1',
+        icon: 'uil uil-document-info',
+        clase: 'cuadro-1',
+        detalle: '23 Enero - 12h a 22h',
+        route: ['/','proyecto-1']
+      },
+      {
+        name: 'Green Webindow 2030: Chan Chan: Identidad Cultural y la preservación de los Valores en la Actualidad. ',
+        imagen: 'https://www.dropbox.com/scl/fi/x9fgwkayjyrgqsgts6r5y/PROYECTO-2.png?rlkey=pqh3jt7pyynftntc7fv332xdx&st=xjprlw3w&raw=1',
+        icon: 'uil uil-document-info',
+        clase: 'cuadro-2',
+        detalle: '27 Enero - 15h - 17h',
+        route: ['/','proyecto-2']
+      },
+      {
+        name: 'Identidad Universitaria: Un Reflejo de su Comunidad - Presentación: Proyecto: "Green Webindow 2030".  Dpto. Idiomas, Universidad Nacional de Trujillo,Perú',
+        imagen: 'https://www.dropbox.com/scl/fi/9jb2u7m0uqf6t4nu428k5/PROYECTO-3.png?rlkey=r8zc5bqzpm4kyl0tcrt5byvbw&st=0zlx43rw&raw=1',
+        icon: 'uil uil-document-info',
+        clase: 'cuadro-3',
+        detalle: '3 de Febrero - 15h 45 ?',
+        route: ['/','proyecto-3']
+      },
+      {
+        name: 'Green Webindow 2030:  La Lengua Materna para el desarrollo personal y la convivencia Propuesta método “SPENAC”',
+        imagen: 'https://www.dropbox.com/scl/fi/tow847g5v41rciuv3wxnv/PROYECTO-4.png?rlkey=xz27vb9nij6x5357j7viuk59d&st=as2q03zh&raw=1',
+        icon: 'uil uil-document-info',
+        clase: 'cuadro-4',
+        detalle: '10 de Febrero - 15h - 17h',
+        route: ['/','proyecto-4']
+      },
     ]
   }
 }
