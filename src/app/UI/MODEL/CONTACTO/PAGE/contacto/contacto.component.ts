3c962469748f653a553945595e5d38d9c8484442
seyalexander <alexande1809piscis@gmail.com>
2025-01-27 01:00:10 -0500

    diseño contactos mejorado

--- src/app/UI/MODEL/CONTACTO/PAGE/contacto/contacto.component.ts
+++ src/app/UI/MODEL/CONTACTO/PAGE/contacto/contacto.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-declare const confetti: any;
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-contacto',
   standalone: true,
@@ -7,29 +7,6 @@ declare const confetti: any;
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
-export class ContactoComponent implements OnInit, AfterViewInit {
-  constructor() {}
+export class ContactoComponent  {
 
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    this.initializeConfetti();
-  }
-
-  initializeConfetti(): void {
-    const runConfetti = document.querySelector('#hs-run-on-click-run-confetti');
-    if (runConfetti) {
-      runConfetti.addEventListener('click', () => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: {
-            y: 0.6,
-          },
-        });
-      });
-    } else {
-      console.error('Element #hs-run-on-click-run-confetti not found');
-    }
-  }
 }
