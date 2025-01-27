2b0923fe6bcb9e0d30bab0c6d624c303d78ed413
seyalexander <alexande1809piscis@gmail.com>
2025-01-27 00:49:27 -0500

    correccion enlace proyecto 4

--- src/app/UI/MODEL/CONTACTO/PAGE/contacto/contacto.component.ts
+++ src/app/UI/MODEL/CONTACTO/PAGE/contacto/contacto.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+declare const confetti: any;
 @Component({
   selector: 'app-contacto',
   standalone: true,
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.initializeConfetti();
+  }
+
+  initializeConfetti(): void {
+    const runConfetti = document.querySelector('#hs-run-on-click-run-confetti');
+    if (runConfetti) {
+      runConfetti.addEventListener('click', () => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: {
+            y: 0.6,
+          },
+        });
+      });
+    } else {
+      console.error('Element #hs-run-on-click-run-confetti not found');
+    }
+  }
 }
